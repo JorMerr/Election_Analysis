54af818afcaa87f5fd07f559792d588e87da8c9b
cookandcrafter <98176999+JorMerr@users.noreply.github.com>
2022-03-24 14:54:35 -0400

    Added largest county data saved to .txt file

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -104,7 +104,7 @@ with open(file_to_save, "w") as txt_file:
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if (county_vote_count > largest_county_count) and (county_percentage > largest_county_percentage):
             largest_county_count = county_vote_count
@@ -113,7 +113,7 @@ with open(file_to_save, "w") as txt_file:
 
     # 7: Print the county with the largest turnout to the terminal.
     largest_county_summary = (
-        f"-------------------------\n"
+        f"\n-------------------------\n"
         f"Largest County Turnout: {largest_county}\n"
         #f"Largest County Turnout Vote Count: {largest_county_count:,}\n"
         #f"Largest County Turnout Percentage of Votes: {largest_county_percentage:.1f}%\n"
@@ -121,7 +121,7 @@ with open(file_to_save, "w") as txt_file:
     print(largest_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
